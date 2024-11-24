52e741cd6d710db5ac7fccfe63e01b7cd8737b20
Qingchen_Jia <879484952@qq.com>
2024-11-24 22:53:26 +0800

    Print Url to console in order to quick-start

--- src/main/java/reggietakeout/ReggieTakeOutApplication.java
+++ src/main/java/reggietakeout/ReggieTakeOutApplication.java
@@ -1,13 +1,23 @@
 package reggietakeout;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ReggieTakeOutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReggieTakeOutApplication.class, args);
+        log.info("""
+                                
+                                
+                -----------------------------------------------------------------------
+                \t项目 {} 启动中......
+                \t后台:\thttp://localhost:9110/backend/page/login/login.html
+                \t客户:\thttp://localhost:9110/front/page/login.html
+                -----------------------------------------------------------------------""", "ReggieTakeOut");
     }
 
 }
